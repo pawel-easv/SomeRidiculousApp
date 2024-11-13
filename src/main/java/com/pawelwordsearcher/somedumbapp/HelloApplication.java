b49a3d188044028e07a55237ec8fdee41a3f4414
Pawel <69588920+pawel-easv@users.noreply.github.com>
2024-11-13 10:24:28 +0100

    init
    # Conflicts:
    #	src/main/java/com/pawelwordsearcher/somedumbapp/HelloApplication.java
    
    # Conflicts:
    #	src/main/java/com/pawelwordsearcher/somedumbapp/HelloApplication.java

--- src/main/java/com/pawelwordsearcher/somedumbapp/HelloApplication.java
+++ src/main/java/com/pawelwordsearcher/somedumbapp/HelloApplication.java
@@ -16,6 +16,13 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
+    public class horse {}
+
+    public class dog {}
+
+    public class cat {}
+
     public static void main(String[] args) {
     System.out.println("Hello World!");
         launch();
