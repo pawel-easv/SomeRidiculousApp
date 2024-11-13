248bf6551473e6f334e8865c004595aafccc8d2b
Pawel <69588920+pawel-easv@users.noreply.github.com>
2024-11-13 12:08:12 +0100

    changes to hello.java

--- src/main/java/com/pawelwordsearcher/somedumbapp/Hello.java
+++ src/main/java/com/pawelwordsearcher/somedumbapp/Hello.java
@@ -3,6 +3,7 @@ package com.pawelwordsearcher.somedumbapp;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Skot mi na kokot");
+        System.out.println("Skot mi na kokot curaku");
         System.out.println("t");
     }
 }
