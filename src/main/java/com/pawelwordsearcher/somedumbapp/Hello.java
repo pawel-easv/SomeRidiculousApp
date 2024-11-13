026534e37e101de006f1b10943c550cb56dee849
pieeernik <igopie01@easv365.dk>
2024-11-13 11:01:58 +0100

    changing sout

--- src/main/java/com/pawelwordsearcher/somedumbapp/Hello.java
+++ src/main/java/com/pawelwordsearcher/somedumbapp/Hello.java
@@ -3,5 +3,6 @@ package com.pawelwordsearcher.somedumbapp;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Skot mi na kokot");
+        System.out.println("t");
     }
 }
